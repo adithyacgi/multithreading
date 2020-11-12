5f6378e723f559dd15a0e2828182bbb7bb69fcf3
adithyacgi <sun.adithya@gmail.com>
2020-11-11 19:29:19 -0600

    Update TestFile.java

--- src/TestFile.java
+++ src/TestFile.java
@@ -4,6 +4,7 @@ public class TestFile {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Added ");
+		System.out.println("chnged");
 		System.out.println("Change from cmd");
 	}
 
